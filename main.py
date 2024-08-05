c574922b0678c88ff64131852760c63f733753ff
StevenDelval <asteven59220@free.fr>
2024-08-05 10:23:04 +0200

    use router

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
-from datetime import datetime, timedelta
-from fastapi import FastAPI, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from pydantic import BaseModel
-from database import SessionLocal, engine
-from models import Base, User
-import crud, auth, schemas
+from fastapi import FastAPI, Depends
+from fastapi.params import Depends
+from models import Base
+from database import engine
+import user_router
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# routes
+# PROTECTED = [Depends(has_access)]
 
-@app.post("/token")
-def login(form_data: schemas.UserCreate, db: Session = Depends(get_db)):
-    user = crud.get_user(db, form_data.username)
-    if not user or not auth.verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
-    access_token_expires = timedelta(minutes=30)
-    access_token = auth.create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}
-
-@app.post("/users/", response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user.username)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
-    return crud.create_user(db=db, user=user)
+app.include_router(
+    user_router.router,
+    prefix="/user",
+)
