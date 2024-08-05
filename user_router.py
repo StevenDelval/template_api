6ac231109d8fa3641f1470f0a0be69ef46be63a1
StevenDelval <asteven59220@free.fr>
2024-08-05 10:22:32 +0200

    add a user router

---
+++ user_router.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from database import get_db
+from models import Base, User
+import crud, auth, schemas
+
+
+
+router = APIRouter()
+
+
+@router.post("/token")
+def login(form_data: schemas.UserCreate, db: Session = Depends(get_db)):
+    user = crud.get_user(db, form_data.username)
+    if not user or not auth.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    access_token_expires = timedelta(minutes=30)
+    access_token = auth.create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/users/", response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    return crud.create_user(db=db, user=user)
