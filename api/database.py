502d883b41328d10ca3e77b90c09220ff10d2f37
StevenDelval <asteven59220@free.fr>
2024-08-07 09:47:22 +0200

    fix warning

--- api/database.py
+++ api/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 import os 
 from dotenv import load_dotenv
